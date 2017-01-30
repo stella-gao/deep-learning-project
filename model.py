999237c8a3c17ba3e97898a20b07ac870ff15eb6
Stella Gao <stella.gao.89@gmail.com>
2017-01-30 15:54:17 -0500

    Create model.py

---
+++ model.py
@@ -0,0 +1,9 @@
+def create_model():
+    # create model
+    model = Sequential()
+    model.add(Dense(12, input_dim=8, init='uniform', activation='relu'))
+    model.add(Dense(8, init='uniform', activation='relu'))
+    model.add(Dense(1, init='uniform', activation='sigmoid'))
+    # Compile model
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
